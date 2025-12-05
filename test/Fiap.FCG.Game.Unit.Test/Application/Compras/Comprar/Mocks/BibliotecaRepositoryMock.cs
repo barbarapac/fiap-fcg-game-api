5c3a35009547e9e2af8db09ac9de83f3d5812a42
Winicius Torres <rm366458@fiap.com.br>
2025-12-05 20:48:38 -0300

    feat: testes de compra para Application

---
+++ test/Fiap.FCG.Game.Unit.Test/Application/Compras/Comprar/Mocks/BibliotecaRepositoryMock.cs
@@ -0,0 +1,15 @@
+﻿using Fiap.FCG.Game.Domain.Compras;
+using Moq;
+
+namespace Fiap.FCG.Game.Unit.Test.Application.Compras.Comprar.Mocks
+{
+    public class BibliotecaRepositoryMock : Mock<IBibliotecaRepository>
+    {
+        public void ConfigurarUsuarioJaPossuiJogoAsync(bool resultado) =>
+        Setup(x => x.UsuarioJaPossuiJogoAsync(It.IsAny<int>(), It.IsAny<int>())).ReturnsAsync(resultado);
+
+
+        public void GarantirAdicionarAsync() =>
+        Verify(x => x.AdicionarAsync(It.IsAny<BibliotecaJogo>()), Times.AtLeastOnce);
+    }
+}
