f2f3e4490a7e49b9b2bc95c5df69cdbdd0a431c9
Fernando <fernandogomes143@gmail.com>
2025-12-22 20:38:29 -0300

    Ajuste testes

--- test/Fiap.FCG.Game.Unit.Test/WebApi/Jogos/Consultar/Mocks/ConsultaJogoQueryMock.cs
+++ test/Fiap.FCG.Game.Unit.Test/WebApi/Jogos/Consultar/Mocks/ConsultaJogoQueryMock.cs
@@ -27,4 +27,59 @@ public class ConsultaJogoQueryMock : Mock<IConsultaJogoQuery>
     {
         Verify(c => c.ObterTodosAsync(), Times.Once);
     }
+
+    public void ConfigurarObterPorNomeOrdenado(string nome, string tipo, bool crescente, string jogos)
+    {
+        Setup(c => c.ObterPorNomeOrdenadoAsync(nome, tipo, crescente))
+            .ReturnsAsync(jogos);
+    }
+
+    public void GarantirObterPorNomeOrdenadoChamado()
+    {
+        Verify(c => c.ObterPorNomeOrdenadoAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>()), Times.Once);
+    }
+
+    public void ConfigurarObterMetricasPreco(string metricas)
+    {
+        Setup(c => c.ObterMetricasPrecoAsync())
+            .ReturnsAsync(metricas);
+    }
+
+    public void GarantirObterMetricasPrecoChamado()
+    {
+        Verify(c => c.ObterMetricasPrecoAsync(), Times.Once);
+    }
+
+    public void ConfigurarObterContagemPorTipo(string contagem)
+    {
+        Setup(c => c.ObterContagemPorTipoAsync())
+            .ReturnsAsync(contagem);
+    }
+
+    public void GarantirObterContagemPorTipoChamado()
+    {
+        Verify(c => c.ObterContagemPorTipoAsync(), Times.Once);
+    }
+
+    public void ConfigurarObterJogosMaisCarosOuBaratos(bool maisCaros, int quantidade, string jogos)
+    {
+        Setup(c => c.ObterJogosMaisCarosOuBaratosAsync(maisCaros, quantidade))
+            .ReturnsAsync(jogos);
+    }
+
+    public void GarantirObterJogosMaisCarosOuBaratosChamado()
+    {
+        Verify(c => c.ObterJogosMaisCarosOuBaratosAsync(It.IsAny<bool>(), It.IsAny<int>()), Times.Once);
+    }
+
+    public void ConfigurarObterPorTipoEPreco(string tipo, double precoMin, double precoMax, string jogos)
+    {
+        Setup(c => c.ObterPorTipoEPrecoAsync(tipo, precoMin, precoMax))
+            .ReturnsAsync(jogos);
+    }
+
+    public void GarantirObterPorTipoEPrecoChamado()
+    {
+        Verify(c => c.ObterPorTipoEPrecoAsync(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>()), Times.Once);
+    }
 }
